540abbc4f23cebce0c8636fc3a45fcf1ed4fbe30
svitlanaburlak <sfetlana.b@gmail.com>
2022-10-28 13:51:13 +0200

    start: yarn, webpack, font, favicon

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+// == Import
+import './styles.scss';
+
+// == Composant
+function App() {
+  return (
+    <div className="app">
+      <h1>List</h1>
+    </div>
+  );
+}
+
+// == Export
+export default App;
